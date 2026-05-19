5c201456fe877e4e68193d08a3e6976be64a0393
Patrick Solamillo <patricksolamillo@proton.me>
2026-05-19 18:47:05 -0400

    add timer control event handler with action mapping
    - add data-action attributes to timer control buttons
    
    - action object to map button clicks to correct handlers
    
    - placeholder functions created for the restart,pause and continue actions.

--- pomododoTimer.js
+++ pomododoTimer.js
@@ -2,7 +2,25 @@ let timerMode = "Standard";
 
 const countdownTimerDisplay = document.querySelector(".timer__display p");
 
+const buttonTimerControls = document.querySelectorAll(".timer__controls button");
+
 const buttonTypeSelector = document.querySelectorAll(".timer__type-selection button");
+
+function handleRestart(){
+    console.log("restart logic handler");
+}
+function handlePause(){
+    console.log("pause logic handler");
+}
+function handleContinue(){
+    console.log("continue logic handler");
+}
+
+const actions = {
+    Restart: handleRestart,
+    Pause: handlePause,
+    Continue: handleContinue,
+}
 
 console.log(buttonTypeSelector);
 buttonTypeSelector.forEach(button => {
@@ -11,3 +29,14 @@ buttonTypeSelector.forEach(button => {
         console.log(`Timer Mode set to ${timerMode}`)
     })
 })
+
+buttonTimerControls.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let currentButton = actions[e.target.dataset.action];
+
+        if (currentButton) {
+            currentButton();
+        }
+    })
+})
+
